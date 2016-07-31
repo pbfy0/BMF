3d672e51d073e154b1d9f1e953430f48c7844838
Paul F-Y <pbfy00@gmail.com>
2016-07-31 02:21:32 -0400

    fixed installer backup

--- installer/install.ps1
+++ installer/install.ps1
@@ -9,10 +9,10 @@ if(!(Test-Path $BrawlhallaPath\mods\ -pathType container)) {
 echo "Copying ModLoader"
 cp bml-core.swf $BrawlhallaPath\mods\
 $xmlpath = "$BrawlhallaPath\META-INF\AIR\application.xml"
-[xml]$ApplicationXml = Get-Content -Path $BrawlhallaPath\META-INF\AIR\application.xml
+[xml]$ApplicationXml = Get-Content -Path $xmlpath
 if($ApplicationXml.application.initialWindow.content -eq "BrawlhallaAir.swf") {
 	echo "Patching XML"
-	cp $xmlpath $xmlpath.bak
+	cp $xmlpath "${xmlpath}.bak"
 	$ApplicationXml.application.initialWindow.content = "bml-launcher.swf"
 	$ApplicationXml.save($xmlpath)
 }
